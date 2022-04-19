7fcb93e211a4eb7ff3066c932065901c4528c059
Gure-san <azrarafani@gmail.com>
2022-04-19 15:06:15 +0700

    awal push repo

---
+++ js/style.js
@@ -0,0 +1,20 @@
+// Navbar Button
+const btnToggle = document.querySelector('.btn-nt');
+const NavbarCollapse = document.querySelector('.navbar-collapse');;
+btnToggle.addEventListener('change', () => {
+    NavbarCollapse.classList.toggle('navbar-animate');
+});
+
+// Dropdown Mobile Version 
+const btnDropdown = document.querySelectorAll('.nav-item.costom .nav-link');
+btnDropdown.forEach( elmnt => {
+    elmnt.addEventListener('click', e => {
+        e.target.nextElementSibling.classList.toggle('nid-animate');
+    });
+});
+
+// Scroll To Top Button
+const stpBtn = document.querySelector('.stp-btn');
+stpBtn.addEventListener('click', () => window.scrollTo({top:0}));
+window.addEventListener('scroll', () => stpBtn.classList.toggle('active', window.scrollY > 500));
+
