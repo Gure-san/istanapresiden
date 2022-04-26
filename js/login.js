cda8c6ccf33d9e7f299ccd7a19c75e251452092f
Gure-san <azrarafani@gmail.com>
2022-04-26 14:25:17 +0700

    membuat halaman login yang responsive

--- js/login.js
+++ js/login.js
@@ -1,6 +1,16 @@
+// Dekstop Layout
+
 const regBtn = document.getElementById('regBtn');
 const logBtn = document.getElementById('logBtn');
 const container = document.getElementById('container');
 
 regBtn.addEventListener('click', () => container.classList.add('right-panel-active'));
 logBtn.addEventListener('click', () => container.classList.remove('right-panel-active'));
+
+
+// Mobile Layout
+const tbReg = document.querySelector('.tb-reg-container a');
+const tbLog = document.querySelector('.tb-log-container a');
+
+tbReg.addEventListener('click', () => container.classList.add('reg-form-active'));
+tbLog.addEventListener('click', () => container.classList.remove('reg-form-active'));
