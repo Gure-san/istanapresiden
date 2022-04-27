fb41dd21abe8a6681548899ff44cd0b77a21707c
Gure-san <azrarafani@gmail.com>
2022-04-27 10:30:37 +0700

    menyelesaikan bug dan tampilan halaman login dan registrasi serta menambahkan favicon pada browser

--- js/login.js
+++ js/login.js
@@ -1,16 +1,27 @@
 // Dekstop Layout
-
 const regBtn = document.getElementById('regBtn');
 const logBtn = document.getElementById('logBtn');
 const container = document.getElementById('container');
 
+if( window.innerWidth > 786 && !container.classList.contains('right-panel-active') ) {
+    container.classList.add('righ-panel-active');
+}
+
 regBtn.addEventListener('click', () => container.classList.add('right-panel-active'));
-logBtn.addEventListener('click', () => container.classList.remove('right-panel-active'));
+logBtn.addEventListener('click', () => {
+    const hash = window.location.hash.replace('#', '');
+    (hash != '') ? location.hash = '' : '';
+    container.classList.remove('right-panel-active');
+});
 
 
 // Mobile Layout
 const tbReg = document.querySelector('.tb-reg-container a');
 const tbLog = document.querySelector('.tb-log-container a');
 
-tbReg.addEventListener('click', () => container.classList.add('reg-form-active'));
+if( window.innerWidth <= 786 && window.location.hash === '#reg') {
+    container.classList.add('reg-form-active');
+}
+
+tbReg.addEventListener('click', (e) => {e.preventDefault(); container.classList.add('reg-form-active')});
 tbLog.addEventListener('click', () => container.classList.remove('reg-form-active'));
